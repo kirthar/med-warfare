0ac6a16a39096f6668612781e4726b214243c8da
Chris Fuertes <chris.fuertes@gmail.com>
2013-08-18 21:18:26 +0200

    Defend skill fixed for no crash

--- app/models/skills/defend.rb
+++ app/models/skills/defend.rb
@@ -2,7 +2,7 @@ module Skills
   class Defend < BaseSkill
     class << self
       def skill(unit, target)
-
+        {unit: {id: unit.id, name: unit.name}, target: { id: target.id, name: target.name, dead: target.dead? }}
       end
     end
   end
