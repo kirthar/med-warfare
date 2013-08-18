1f1ca519c5e2571a2376d09e736b88a0a3f83b4d
Chris Fuertes <chris.fuertes@gmail.com>
2013-08-18 23:27:43 +0200

    Added username to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130818100411) do
+ActiveRecord::Schema.define(:version => 20130818211950) do
 
   create_table "combat_actions", :force => true do |t|
     t.integer  "unit_id"
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(:version => 20130818100411) do
   create_table "combats", :force => true do |t|
     t.integer  "current_unit_id"
     t.string   "status"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   add_index "combats", ["status"], :name => "index_combats_on_status"
@@ -94,9 +94,11 @@ ActiveRecord::Schema.define(:version => 20130818100411) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
