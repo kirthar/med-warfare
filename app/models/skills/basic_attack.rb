6628059a8513cdd71023054ca0c9ce836bd4bd31
Chris Fuertes <chris.fuertes@gmail.com>
2013-08-18 13:45:11 +0200

    When a unit uses a skill, changes to the next unit

--- app/models/skills/basic_attack.rb
+++ app/models/skills/basic_attack.rb
@@ -2,7 +2,15 @@ module Skills
   class BasicAttack < BaseSkill
     class << self
       def skill(unit, target)
-        unit.do_damage_to(target, Unit::BASE_DMG, unit.default_dmg_type)
+        attack_roll = rand(100)
+        defense_roll = rand(100)
+        damage = Unit::BASE_DMG
+        if attack_roll <= unit.hit_chance and defense_roll > target.evade_chance
+          damage *= critical_damage if attack_roll <= unit.critical_chance
+          unit.do_damage_to(target, damage, unit.default_dmg_type)
+        else
+          miss(unit, target)
+        end
       end
     end
   end
